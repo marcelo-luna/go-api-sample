098116d417f40650d5339cf769ade2b61421a69f
Marcelo Luna <HQLUNAMA@carlsberggroup.com>
2025-09-24 19:14:38 +0200

    create ping endpoint

--- web-service-gin/src/main.go
+++ web-service-gin/src/main.go
@@ -10,13 +10,14 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/ping", getPing)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	ConnectDatabase()
 	//defer Db.Close()
 
-	//router.Run("localhost:8080") //no docker
+	//router.Run("localhost:8883") //no docker
 	router.Run(":8080") //docker
 }
 
@@ -33,6 +34,11 @@ var albums = []album{
 	{ID: 1, Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: 2, Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: 3, Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getPing responds with pong.
+func getPing(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "pong")
 }
 
 // getAlbums responds with the list of all albums as JSON.
